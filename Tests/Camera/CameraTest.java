b50f47f1d0cb3550703961c226ebe33e2a7eb8c7
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2018-05-07 16:31:53 +0200

    Add test for camera Change name from cameraBox to cameraChest Include jUnit in maven

---
+++ Tests/Camera/CameraTest.java
@@ -0,0 +1,22 @@
+package Camera;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CameraTest {
+
+	@org.junit.jupiter.api.BeforeEach
+	void setUp() {
+	}
+
+	@org.junit.jupiter.api.AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void constructorTest(){
+		assertNotNull(new Camera());
+	}
+}
--- src/Camera/CameraBox.java
+++
@@ -1,7 +0,0 @@
-package Camera;
-
-/**
- * Class for describing a box found in a camerastream/video/image
- */
-public class CameraBox extends CameraObject {
-}
