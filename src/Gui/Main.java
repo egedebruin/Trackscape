0fbf0c8c6d8b8a58e9d65aa6b703c06482c53e34
Lisette <lisette_stella@hotmail.com>
2018-05-08 15:44:47 +0200

    Create basic GUI that can play a selected video

--- src/Gui/Main.java
+++ src/Gui/Main.java
@@ -1,10 +1,12 @@
 package Gui;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.media.Media;
@@ -16,12 +18,6 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.control.Button;
-import javafx.scene.input.MouseEvent;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.FileChooser;
 
@@ -35,17 +31,18 @@ public class Main extends Application {
         BorderPane mainPane = new BorderPane();      // Choose best-fitted pane for the application
 
         // All panes
-        FlowPane flowPane = new FlowPane();
-        flowPane.setAlignment(Pos.TOP_CENTER);
-        StackPane videoPane = new StackPane();
-        FlowPane flowPane2 = new FlowPane();
+        FlowPane titlePane = new FlowPane();
+        titlePane.setAlignment(Pos.TOP_CENTER);
+        BorderPane videoPane = new BorderPane();
+        FlowPane bottomPane = new FlowPane();
+        StackPane stack = new StackPane();
 
         // Structure of panes inside main pane
-        mainPane.setTop(flowPane);
+        mainPane.setTop(titlePane);
         //mainPane.setLeft(...);
         mainPane.setCenter(videoPane);
         //mainPane.setRight(...);
-        mainPane.setBottom(flowPane2);
+        mainPane.setBottom(bottomPane);
 
         //-------------------- Top of the application (title)-------------
         Text text = new Text("TrackScape");
@@ -54,10 +51,38 @@ public class Main extends Application {
         text.setStroke(Color.LIGHTSLATEGREY);
         text.setStrokeWidth(2);
 
-        flowPane.getChildren().addAll(text);
+        titlePane.getChildren().addAll(text);
 
         //-------------------- Center of the application (mediaplayer)-------------
-        // Working on this atm!
+
+        // The Menu
+        MenuBar video_menuBar = new MenuBar();
+        Menu video_menuFile = new Menu("File");
+        Menu data = new Menu("Overview of Statistics");
+        MenuItem video_open = new MenuItem("Open File...");
+        MenuItem joke = new MenuItem("Robins stukje");
+        video_menuFile.getItems().addAll(video_open, joke);
+        video_menuBar.getMenus().addAll(video_menuFile, data);
+        video_menuBar.prefWidthProperty().bind(videoPane.widthProperty());
+
+        StackPane menu = new StackPane();
+        menu.getChildren().add(video_menuBar);
+        videoPane.setTop(menu);
+
+        // The Mediaplayer
+        MediaView video_mv = new MediaView();
+        stack.getChildren().addAll(new Rectangle(1000, 600, Color.BLACK), video_mv);
+        video_open.setOnAction(t -> {
+            FileChooser chooser = new FileChooser();
+            File file = chooser.showOpenDialog(primaryStage);
+            if (file != null) {
+                Media video_source = new Media(file.toURI().toString());
+                MediaPlayer video_mp = new MediaPlayer(video_source);
+                video_mv.setMediaPlayer(video_mp);
+                video_mp.play();
+            }
+        });
+        videoPane.setBottom(stack);
 
         //-------------------- Bottom of the application (button)-------------
         Text text2 = new Text("© TrackScape");
@@ -65,7 +90,8 @@ public class Main extends Application {
         text2.setFill(Color.BLACK);
         text2.setStroke(Color.LIGHTSLATEGREY);
         text2.setStrokeWidth(1);
-        flowPane2.getChildren().addAll(text2);
+        bottomPane.getChildren().addAll(text2);
+
 
         // Show the scene
         Scene scene = new Scene(mainPane,1250, 800);
