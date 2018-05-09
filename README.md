72ccc4a68f3ad2054e798e4edf566db812d4102e
egedebruin <egedebruin@gmail.com>
2018-05-09 13:54:41 +0200

    Change configuration for OpenCV

--- README.md
+++ README.md
@@ -6,6 +6,3 @@ BEP
 
 * Download OpenCV from https://opencv.org/releases.html
     * For Windows: Choose 3.4.1 Win Pack
-* Extract files self-selected map
-* Copy opencv\build\java\x86\opencv_java341.dll to Trackscape/libs
-* Copy opencv\build\bin\opencv_ffmpeg341.dll to Trackscape/libs
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
+import nu.pattern.OpenCV;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
 import java.io.File;
-import java.lang.reflect.Field;
 
 public class Main {
 
@@ -10,14 +10,10 @@ public class Main {
         String string = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov";
         String file = "files" + File.separator + "webcast.mov";
 
-        // Set property to load OpenCV library
-        System.setProperty("java.library.path", System.getProperty("user.dir") + File.separator + "libs");
-        Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
-        fieldSysPath.setAccessible( true );
-        fieldSysPath.set( null, null );
-
         // Load OpenCV library
-        nu.pattern.OpenCV.loadShared();
+        System.load(System.getProperty("user.dir") + "\\libs\\opencv_ffmpeg341_64.dll");
+        System.load(System.getProperty("user.dir") + "\\libs\\opencv_java341.dll");
+        //OpenCV.loadShared();
 
         Mat frame = new Mat();
         VideoCapture camera = new VideoCapture();
