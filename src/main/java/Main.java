66eac0782b9b27b6204bd4abfe02ea4e4fdc61e2
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2018-05-14 11:07:23 +0200

    resolved checkstyle violations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,32 +3,39 @@ import java.awt.image.DataBufferByte;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import nu.pattern.OpenCV;
+import javax.swing.WindowConstants;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
-import java.io.File;
+
+
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        String string = "rtsp://192.168.0.117:554/user=admin&password=&channel=1&stream=1.sdp?real_stream--rtp-caching=100";
-        String file = "files" + File.separator + "webcast.mov";
-
+    /**
+     * Main method of this class.
+     * @param args arguments
+     */
+    public static void main(String[] args) {
         // Load OpenCV library
         System.load(System.getProperty("user.dir") + "\\libs\\opencv_ffmpeg341_64.dll");
         System.load(System.getProperty("user.dir") + "\\libs\\opencv_java341.dll");
         //OpenCV.loadShared();
 
-        Mat frame = new Mat();
-        VideoCapture camera = new VideoCapture();
-        System.out.println(camera.open(string));
-
+        //Creates GUI window
         JFrame jframe = new JFrame("Robins stukje");
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         JLabel vidpanel = new JLabel();
         jframe.setContentPane(vidpanel);
         jframe.setVisible(true);
+
+        //link to a live streaming camera
+        String string = "rtsp://192.168.0.117:554/"
+            + "user=admin&password=&channel=1&stream=1.sdp?real_stream--rtp-caching=100";
+
+        Mat frame = new Mat();
+        VideoCapture camera = new VideoCapture();
+        System.out.println(camera.open(string));
 
         while (camera.read(frame)) {
             ImageIcon image = new ImageIcon(toBufferedImage(frame));
@@ -37,9 +44,10 @@ public class Main {
         }
     }
 
-    static BufferedImage toBufferedImage(Mat videoMatImage) {
+    private static BufferedImage toBufferedImage(Mat videoMatImage) {
 
-        int type = videoMatImage.channels() == 1 ? BufferedImage.TYPE_BYTE_GRAY : BufferedImage.TYPE_3BYTE_BGR;
+        int type = videoMatImage.channels() == 1
+            ? BufferedImage.TYPE_BYTE_GRAY : BufferedImage.TYPE_3BYTE_BGR;
         int bufferSize = videoMatImage.channels() * videoMatImage.cols() * videoMatImage.rows();
         byte[] buffer = new byte[bufferSize];
         videoMatImage.get(0, 0, buffer);
