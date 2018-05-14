aa64556b74b8a43ced277f6331402ef71b6008f9
egedebruin <egedebruin@gmail.com>
2018-05-14 12:39:51 +0200

    Do not run test when not on windows

--- src/test/java/camera/CameraTest.java
+++ src/test/java/camera/CameraTest.java
@@ -3,26 +3,29 @@ package camera;
 import java.io.File;
 import org.junit.jupiter.api.Test;
 
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class CameraTest {
 
-	@org.junit.jupiter.api.BeforeEach
-	void setUp() {
-		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
-			System.load(System.getProperty("user.dir")
-				+ File.separator + "libs" + File.separator + "opencv_ffmpeg341_64.dll");
-			System.load(System.getProperty("user.dir")
-				+ File.separator + "libs" + File.separator + "opencv_java341.dll");
-		}
-	}
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            System.load(System.getProperty("user.dir")
+                + File.separator + "libs" + File.separator + "opencv_ffmpeg341_64.dll");
+            System.load(System.getProperty("user.dir")
+                + File.separator + "libs" + File.separator + "opencv_java341.dll");
+        }
+    }
 
-	@org.junit.jupiter.api.AfterEach
-	void tearDown() {
-	}
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
 
-	@Test
-	void constructorTest(){
-		assertNotNull(new Camera(null, null));
-	}
+    @Test
+    void constructorTest() {
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            assertNotNull(new Camera(null, null));
+        }
+    }
 }
