2ece24777c0d13f0c8571e3a1fd91365f96e7011
egedebruin <egedebruin@gmail.com>
2018-05-14 11:38:32 +0200

    Change slash to File.separator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 public class Main {
     /**
      * Skeleton main.
@@ -12,8 +14,10 @@ public class Main {
         // These should be at the start of the application,
         // so if the main changes this should be included.
         // Load OpenCV library.
-        System.load(System.getProperty("user.dir") + "\\libs\\opencv_ffmpeg341_64.dll");
-        System.load(System.getProperty("user.dir") + "\\libs\\opencv_java341.dll");
+        System.load(System.getProperty("user.dir")
+            + File.separator + "libs" + File.separator + "opencv_ffmpeg341_64.dll");
+        System.load(System.getProperty("user.dir")
+            + File.separator + "libs" + File.separator + "opencv_java341.dll");
 
     }
 }
--- src/test/java/camera/CameraTest.java
+++ src/test/java/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package camera;
 
+import java.io.File;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -8,8 +9,10 @@ class CameraTest {
 
 	@org.junit.jupiter.api.BeforeEach
 	void setUp() {
-		System.load(System.getProperty("user.dir") + "\\libs\\opencv_ffmpeg341_64.dll");
-		System.load(System.getProperty("user.dir") + "\\libs\\opencv_java341.dll");
+		System.load(System.getProperty("user.dir")
+			+ File.separator + "libs" + File.separator + "opencv_ffmpeg341_64.dll");
+		System.load(System.getProperty("user.dir")
+			+ File.separator + "libs" + File.separator + "opencv_java341.dll");
 	}
 
 	@org.junit.jupiter.api.AfterEach
