d5f97292c0b61b3f2df35c1118be37c7c0e13566
egedebruin <egedebruin@gmail.com>
2018-05-15 16:35:31 +0200

    Faster background subtractor

--- src/main/java/handlers/CameraHandler.java
+++ src/main/java/handlers/CameraHandler.java
@@ -4,7 +4,9 @@ import camera.Camera;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.video.BackgroundSubtractorKNN;
 import org.opencv.video.BackgroundSubtractorMOG2;
 import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
@@ -18,7 +20,7 @@ public class CameraHandler {
      * The list of the cameras.
      */
     private List<Camera> cameraList;
-
+    
     /**
      * Constructor for the CameraHandler class.
      */
@@ -52,25 +54,12 @@ public class CameraHandler {
         if (camera.getFirstFrame() == null) {
             camera.setFirstFrame(newFrame);
         }
-        return getSubtraction(newFrame, camera);
+        return getSubtraction(newFrame, camera.getFirstFrame());
     }
 
-    public Mat getSubtraction(Mat frame, Camera camera) {
-        Mat background = camera.getFirstFrame();
-        Mat result = frame.clone();
-        ArrayList list = new ArrayList();
-        IntStream.range(0, background.cols()).parallel().forEach(i ->
-            IntStream.range(0, background.rows()).parallel().forEach(j -> {
-            boolean different = false;
-            for (int k = 0; k < background.get(j, i).length; k++) {
-                if (Math.abs(background.get(j, i)[k] - frame.get(j, i)[k]) > 20) {
-                    different = true;
-                }
-            }
-            if (!different) {
-                result.put(j, i, 0, 0, 0);
-            }
-        }));
+    public Mat getSubtraction(Mat frame, Mat background) {
+        Mat result = new Mat();
+        Core.subtract(frame, background, result);
         return result;
     }
 
