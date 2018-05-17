b30298164c63ea392047b39544843209e20ec814
egedebruin <egedebruin@gmail.com>
2018-05-17 11:15:52 +0200

    Detect peepz

--- src/main/java/handlers/CameraHandler.java
+++ src/main/java/handlers/CameraHandler.java
@@ -3,11 +3,17 @@ package handlers;
 import camera.Camera;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.HOGDescriptor;
 import org.opencv.video.BackgroundSubtractorKNN;
-import org.opencv.video.BackgroundSubtractorMOG2;
 import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
 
@@ -20,16 +26,23 @@ public class CameraHandler {
      * The list of the cameras.
      */
     private List<Camera> cameraList;
-    
+
+    private HOGDescriptor hogDescriptor = new HOGDescriptor();
+
+    private BackgroundSubtractorKNN knn =
+        Video.createBackgroundSubtractorKNN(1, 1000, true);
+
     /**
      * Constructor for the CameraHandler class.
      */
     public CameraHandler() {
         cameraList = new ArrayList<>();
+        hogDescriptor.setSVMDetector(HOGDescriptor.getDefaultPeopleDetector());
     }
 
     /**
      * Add a new camera to use for streaming.
+     *
      * @param link The link of the camera.
      * @return The new camera as a Camera object.
      */
@@ -46,6 +59,7 @@ public class CameraHandler {
 
     /**
      * Get a new frame from the camera.
+     *
      * @param camera The camera to get the new frame from.
      * @return The new frame as a BufferedImage.
      */
@@ -54,17 +68,39 @@ public class CameraHandler {
         if (camera.getFirstFrame() == null) {
             camera.setFirstFrame(newFrame);
         }
+
         return getSubtraction(newFrame, camera.getFirstFrame());
     }
 
     public Mat getSubtraction(Mat frame, Mat background) {
+        MatOfRect rects = new MatOfRect();
+        MatOfDouble matOfDouble = new MatOfDouble();
+
         Mat result = new Mat();
-        Core.subtract(frame, background, result);
-        return result;
+
+        knn.apply(frame, result);
+
+        List<Rect> list = rects.toList();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (matOfDouble.toList().get(i) > 1) {
+                System.out.println(matOfDouble.toList().get(i));
+                Rect rectangle = list.get(i);
+                Point point1 = new Point();
+                Point point2 = new Point();
+                point1.x = rectangle.x;
+                point1.y = rectangle.y;
+                point2.x = rectangle.x + rectangle.width;
+                point2.y = rectangle.y + rectangle.height;
+                Imgproc.rectangle(frame, point1, point2, new Scalar(0, 255, 0), 2);
+            }
+        }
+        return frame;
     }
 
     /**
      * Get the size of the list of cameras.
+     *
      * @return The size of the list.
      */
     public int listSize() {
@@ -80,6 +116,7 @@ public class CameraHandler {
 
     /**
      * Get a camera from the list.
+     *
      * @param index The index of the camera.
      * @return The camera.
      */
