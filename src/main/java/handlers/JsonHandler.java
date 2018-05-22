e4569145e978681a9833312b40fa09060971a1de
egedebruin <egedebruin@gmail.com>
2018-05-22 14:27:22 +0200

    Add methods to JsonHandler and create tests

--- src/main/java/handlers/JsonHandler.java
+++ src/main/java/handlers/JsonHandler.java
@@ -1,16 +1,29 @@
 package handlers;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Class for handling Json config files.
+ */
 public class JsonHandler {
 
+    /**
+     * Class variables.
+     */
     private JsonElement jsonElement;
 
+    /**
+     * Constructor for JsonHandler.
+     * @param fileName The name of the json config file.
+     */
     public JsonHandler(String fileName) {
         File file = new File(fileName);
         JsonParser parser = new JsonParser();
@@ -18,7 +31,66 @@ public class JsonHandler {
             BufferedReader reader = new BufferedReader(new FileReader(file));
             jsonElement = parser.parse(reader);
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            jsonElement = null;
         }
+    }
+
+    /**
+     * Get the links to the cameras of the room.
+     * @param roomId The Id of the room.
+     * @return List of the links to the cameras.
+     */
+    public List<String> getCameraLinks(int roomId) {
+        JsonElement room = getRoomById(roomId);
+        List<String> cameras = new ArrayList<>();
+        JsonElement element = room.getAsJsonObject().get("cameras");
+        for (JsonElement camera : element.getAsJsonArray()) {
+            String link = camera.getAsJsonObject().get("link").getAsString();
+            cameras.add(link);
+        }
+        return cameras;
+    }
+
+    /**
+     * Get the amount of people in the room.
+     * @param roomId The id of the room.
+     * @return The amount of people.
+     */
+    public int getAmountPeople(int roomId) {
+        JsonElement room = getRoomById(roomId);
+        return room.getAsJsonObject().get("people").getAsInt();
+    }
+
+    /**
+     * Get the amount of chests in the room.
+     * @param roomId The id of the room.
+     * @return The amount of chests.
+     */
+    public int getAmountChests(int roomId) {
+        JsonElement room = getRoomById(roomId);
+        return room.getAsJsonObject().get("chests").getAsInt();
+    }
+
+    /**
+     * Get the Json element of a room.
+     * @param roomId The id of the room.
+     * @return The Json element of the room.
+     */
+    private JsonElement getRoomById(int roomId) {
+        JsonArray array = jsonElement.getAsJsonArray();
+        for (JsonElement room : array) {
+            if (room.getAsJsonObject().get("roomId").getAsInt() == roomId) {
+                return room;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Get the json element.
+     * @return The json element.
+     */
+    public JsonElement getJsonElement() {
+        return jsonElement;
     }
 }
---
+++ src/test/java/handlers/JsonHandlerTest.java
@@ -0,0 +1,79 @@
+package handlers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the Json handler class.
+ */
+public class JsonHandlerTest {
+
+    /**
+     * Class variables.
+     */
+    private final String jsonFile = "files/example.json";
+    private JsonHandler handler;
+
+    /**
+     * Test the constructor, check if the file is read correctly.
+     */
+    @Test
+    void constructorTest() {
+        handler = new JsonHandler(jsonFile);
+        assertNotNull(handler.getJsonElement());
+    }
+
+    /**
+     * Test the getCameraLinks method.
+     */
+    @Test
+    void getCameraLinksTest() {
+        handler = new JsonHandler(jsonFile);
+        List<String> list = handler.getCameraLinks(0);
+        assertEquals(2, list.size());
+    }
+
+    /**
+     * Test the getAmountPeople method.
+     */
+    @Test
+    void getAmountPeopleTest() {
+        handler = new JsonHandler(jsonFile);
+        int people = handler.getAmountPeople(0);
+        assertEquals(5, people);
+    }
+
+    /**
+     * Test the getAmountChests method.
+     */
+    @Test
+    void getAmountChestsTest() {
+        handler = new JsonHandler(jsonFile);
+        int chests = handler.getAmountChests(0);
+        assertEquals(3, chests);
+    }
+
+    /**
+     * Test that NullPointer will be thrown if roomId is not correct.
+     */
+    @Test
+    void incorrectRoomIdTest() {
+        handler = new JsonHandler(jsonFile);
+        assertThrows(NullPointerException.class,
+            () -> handler.getAmountChests(2));
+    }
+
+    /**
+     * Test that constructor doesn't create jsonElement with wrong filename.
+     */
+    @Test
+    void incorrectFileNameTest() {
+        handler = new JsonHandler("ajax");
+        assertNull(handler.getJsonElement());
+    }
+}
