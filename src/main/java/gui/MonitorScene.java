bb765f2fe737c19a8116152a70a0011e639d410b
Lisette <lisette_stella@hotmail.com>
2018-05-25 14:33:56 +0200

    Use pushedPlay variable to make sure no new files/configurations can be loaded if there are already videos playing

--- src/main/java/gui/MonitorScene.java
+++ src/main/java/gui/MonitorScene.java
@@ -41,6 +41,7 @@ public class MonitorScene extends BaseScene {
         + ".sdp?real_stream--rtp-caching=100";
     private FlowPane mediaPlayerPane = new FlowPane();
     private Label cameraStatus;
+    private boolean pushedPlay = false;
 
     /**
      * Constructor.
@@ -248,7 +249,8 @@ public class MonitorScene extends BaseScene {
         playButton.setOnAction(event -> {
             if (getController().getCameras() == 0) {
                 showCameraIcon();
-            } else {
+            } else if (!pushedPlay) {
+                pushedPlay = true;
                 initializeImageViewers();
                 getController().grabTimeFrame(imageViews);
             }
@@ -311,12 +313,15 @@ public class MonitorScene extends BaseScene {
      */
     private void openConfig(final MenuItem configFile, final Stage primaryStage) {
         configFile.setOnAction(event -> {
-            FileChooser chooser = new FileChooser();
-            chooser.setTitle("Select Configuration File (JSon format)");
-            File file = chooser.showOpenDialog(primaryStage);
-            JsonHandler jsonHandler = new JsonHandler(file.toString());
-            getController().configure(jsonHandler);
-            setCameraStatus();
+            if (!pushedPlay) {
+                FileChooser chooser = new FileChooser();
+                chooser.setTitle("Select Configuration File (JSon format)");
+                File file = chooser.showOpenDialog(primaryStage);
+                JsonHandler jsonHandler = new JsonHandler(file.toString());
+                getController().configure(jsonHandler);
+                setCameraStatus();
+
+            }
         });
     }
 
@@ -326,9 +331,11 @@ public class MonitorScene extends BaseScene {
      */
     private void standardConfig(final MenuItem standardFile) {
         standardFile.setOnAction(event -> {
-            JsonHandler jsonHandler = new JsonHandler("files/standard.json");
-            getController().configure(jsonHandler);
-            setCameraStatus();
+            if (!pushedPlay) {
+                JsonHandler jsonHandler = new JsonHandler("files/standard.json");
+                getController().configure(jsonHandler);
+                setCameraStatus();
+            }
         });
     }
 
@@ -341,12 +348,14 @@ public class MonitorScene extends BaseScene {
     private void openVideo(final MenuItem openVideo,
                            final Stage primaryStage) {
         openVideo.setOnAction(t -> {
-            FileChooser chooser = new FileChooser();
-            chooser.setTitle("Select Video File");
-            File file = chooser.showOpenDialog(primaryStage);
-            if (file != null) {
-                getController().createVideo(file);
-                setCameraStatus();
+            if (!pushedPlay) {
+                FileChooser chooser = new FileChooser();
+                chooser.setTitle("Select Video File");
+                File file = chooser.showOpenDialog(primaryStage);
+                if (file != null) {
+                    getController().createVideo(file);
+                    setCameraStatus();
+                }
             }
         });
     }
@@ -360,51 +369,49 @@ public class MonitorScene extends BaseScene {
     private void connectStream(
         final MenuItem connectStream, final Stage primaryStage) {
         connectStream.setOnAction(t -> {
-            // Set up pop up window
-            final Stage streamStage = new Stage();
-            streamStage.initModality(Modality.APPLICATION_MODAL);
-            streamStage.initOwner(primaryStage);
+            if (!pushedPlay) {
+                // Set up pop up window
+                final Stage streamStage = new Stage();
+                streamStage.initModality(Modality.APPLICATION_MODAL);
+                streamStage.initOwner(primaryStage);
 
-            // Set up layout of the pop up window
-            final Label fieldLabel = new Label("Enter url of the RTSP stream:");
-            final TextField field = new TextField();
-            Button submit = new Button("Submit");
+                // Set up layout of the pop up window
+                final Label fieldLabel = new Label("Enter url of the RTSP stream:");
+                final TextField field = new TextField();
+                Button submit = new Button("Submit");
 
-            final int spacing = 6;
-            final int insetPositions = 10;
+                final int spacing = 6;
+                final int insetPositions = 10;
 
-            // Set up box in pop up window
-            VBox popUpVBox = new VBox();
-            popUpVBox.setPadding(new Insets(insetPositions,
-                insetPositions, insetPositions, insetPositions));
-            popUpVBox.getChildren().addAll(fieldLabel, field, submit);
-            popUpVBox.setSpacing(spacing);
-            popUpVBox.setAlignment(Pos.CENTER);
+                // Set up box in pop up window
+                VBox popUpVBox = new VBox();
+                popUpVBox.setPadding(new Insets(insetPositions,
+                    insetPositions, insetPositions, insetPositions));
+                popUpVBox.getChildren().addAll(fieldLabel, field, submit);
+                popUpVBox.setSpacing(spacing);
+                popUpVBox.setAlignment(Pos.CENTER);
 
-            // Save the url of the RTSP stream by clicking on submit
-            submit.setOnAction(t1 -> {
-                if (!getController().getCameraActive()) {
+                // Save the url of the RTSP stream by clicking on submit
+                submit.setOnAction(t1 -> {
                     getController().createStream(streamStage, field);
                     setCameraStatus();
-                }
-            });
+                });
 
-            // Save the url of the RTSP stream by pressing on the enter key
-            field.setOnKeyPressed(keyEvent -> {
-                if (keyEvent.getCode() == KeyCode.ENTER)  {
-                    if (!getController().getCameraActive()) {
+                // Save the url of the RTSP stream by pressing on the enter key
+                field.setOnKeyPressed(keyEvent -> {
+                    if (keyEvent.getCode() == KeyCode.ENTER) {
                         getController().createStream(streamStage, field);
                         setCameraStatus();
                     }
-                }
-            });
+                });
 
-            final int popUpWidth = 500;
-            final int popUpHeight = 100;
+                final int popUpWidth = 500;
+                final int popUpHeight = 100;
 
-            Scene popUp = new Scene(popUpVBox, popUpWidth, popUpHeight);
-            streamStage.setScene(popUp);
-            streamStage.show();
+                Scene popUp = new Scene(popUpVBox, popUpWidth, popUpHeight);
+                streamStage.setScene(popUp);
+                streamStage.show();
+            }
         });
     }
 
@@ -416,11 +423,11 @@ public class MonitorScene extends BaseScene {
     private void theStream(final MenuItem theStream) {
         theStream.setOnAction((ActionEvent t)
             -> {
-            if (!getController().getCameraActive()) {
+            if (!pushedPlay) {
                 getController().createTheStream(theStreamString);
                 setCameraStatus();
             }
-        });
+            });
     }
 
     /**
@@ -461,6 +468,7 @@ public class MonitorScene extends BaseScene {
         getController().closeStream();
         mediaPlayerPane.getChildren().clear();
         showCameraIcon();
+        pushedPlay = false;
     }
 
 }
