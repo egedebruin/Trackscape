d097ba08f2eb0ef7fc0eb4e6d8c3382a7490c587
Jasper Kroes <J.Kroes-1@student.tudelft.nl>
2018-06-07 16:25:38 +0200

    Added full chest tracking, works pretty well. Sometimes doesnt recognize previous chest, because some people cant paint

--- src/main/java/camera/CameraChestTracker.java
+++ src/main/java/camera/CameraChestTracker.java
@@ -1,19 +1,20 @@
 package camera;
 
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for tracking chests.
+ */
 public class CameraChestTracker {
 
     private Mat previousFrame = new Mat();
-    private static final Scalar CHESTBOXCOLOUR = new Scalar(255, 0, 255);
+    private  List<MatOfPoint> previousContours = new ArrayList<>();
 
     /**
      * Method which removes areas from frame, if they have overlap with the previous frame.
@@ -21,55 +22,66 @@ public class CameraChestTracker {
      * @param minChestArea The minimal area a rect has to be to be counted
      * @return The frame with possible found areas removed
      */
-    public Mat trackChests(final Mat frame, double minChestArea) {
+    public Mat trackChests(final Mat frame, final double minChestArea) {
         Mat tempFrame = frame.clone();
-        if (!previousFrame.size().equals(frame.size())) {
-            previousFrame = frame.clone();
-//            System.out.println("previousframe updated");
 
-        } else {
-            Mat overlappingFrame = new Mat();
+        // calculate the contours in frame
+        List<MatOfPoint> contoursFrame = new ArrayList<>();
+        Mat contourMatFrame = new Mat();
+        Imgproc.findContours(tempFrame, contoursFrame,
+            contourMatFrame, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
 
-            // calculate the overlapping parts between previousFrame and frame
-            Core.bitwise_and(previousFrame, tempFrame, overlappingFrame);
-            System.out.println("previousframe cloned: \n" + Core.countNonZero(previousFrame) + " : " + Core.countNonZero(tempFrame));
-
-            // set frame to be the previousFrame
-//            previousFrame = frame.clone();
-
-
-//            // calculate the contours in overlappingFrame
-//            List<MatOfPoint> contoursOverlappingFrame = new ArrayList<>();
-//            Mat contourMatOverlappingFrame = new Mat();
-//            Imgproc.findContours(overlappingFrame, contoursOverlappingFrame,
-//                contourMatOverlappingFrame, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-
-            // calculate the contours in frame
-            List<MatOfPoint> contoursFrame = new ArrayList<>();
-            Mat contourMatFrame = new Mat();
-            Imgproc.findContours(tempFrame, contoursFrame,
-                contourMatFrame, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_SIMPLE);
-
+        if (previousFrame.size().equals(frame.size())) {
+            // Check if there is overlap between chests in subsequent frames.
             for (MatOfPoint points : contoursFrame) {
                 Rect rect = Imgproc.boundingRect(new MatOfPoint(points.toArray()));
-                if (rect.width * rect.height > minChestArea) {
-//                    Mat checkOverlap = new Mat();
-//                    Core.bitwise_and(tempFrame.submat(rect), overlappingFrame.submat(rect), checkOverlap);
-//                    System.out.println(Core.countNonZero(tempFrame.submat(rect)));
-//                    System.out.println(Core.countNonZero(overlappingFrame.submat(rect)));
-//                    System.out.println(Core.countNonZero(checkOverlap));
-//                    tempFrame.submat(rect).setTo(Mat.zeros(rect.size(), frame.type()));
-                    return tempFrame.submat(rect);
-//                    if (Core.countNonZero(previousFrame.submat(rect)) > 0) {
-////                        frame.submat(rect).setTo(Mat.zeros(rect.size(), frame.type()));
-////                        System.exit(1000);
-//                    }
+                if (rect.area() > minChestArea) {
+                    for (MatOfPoint points2 : previousContours) {
+                        Rect rect2 = Imgproc.boundingRect(new MatOfPoint(points2.toArray()));
+
+                        if (rect2.area() > minChestArea) {
+                            if (doOverlap(rect, rect2)) {
+                                setRectToZerosInFrame(tempFrame, rect);
+                            }
+                        }
+                    }
                 }
             }
-
         }
+
+        previousContours = contoursFrame;
         previousFrame = frame.clone();
+
         return tempFrame;
+    }
+
+    /**
+     * Sets a subMatrix to only zeros (black).
+     * @param tempFrame the frame where an area should be removed from
+     * @param rect the position where zeros should be inserted
+     */
+    private void setRectToZerosInFrame(final Mat tempFrame, final Rect rect) {
+        if (tempFrame.submat(rect).isSubmatrix()) {
+            Mat zeros = Mat.zeros(rect.size(), tempFrame.type());
+            zeros.copyTo(tempFrame.submat(rect));
+        }
+    }
+
+    /**
+     * Method that checks if two Rects overlap.
+     * @param r1    Rect 1
+     * @param r2    Rect 2
+     * @return  true iff rect1 has overlap with r2, false otherwise.
+     */
+    private Boolean doOverlap(final Rect r1, final Rect r2) {
+        if (r1.x + r1.width < r2.x || r1.x > r2.x + r2.width) {
+            return false;
+        }
+        if (r1.y + r1.height < r2.y || r1.y > r2.y + r2.height) {
+            return false;
+        }
+
+        return true;
     }
 
 }
